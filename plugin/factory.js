34f4935a30c994bfc6bb10e28b95590401234bbe
Ardhi Lukianto <ardhi@lukianto.com>
2025-03-22 23:20:22 +0700

    Updates

--- plugin/factory.js
+++ plugin/factory.js
@@ -1,20 +1,14 @@
-import zmq from 'zeromq'
+/*
+* Data published to queue should follow this format
+* { worker, payload, source }
+* worker: a bajo.callHandler callable string/function
+* payload:
+* - type: data type ('string', 'error', 'object', etc)
+* - data: data value
+* source: <ns>.<subNs>:<path>
+*/
 
-async function runner () {
-  const { runHook, callHandler } = this.app.bajo
-  for await (const [msg] of this.puller) {
-    try {
-      const { type, payload } = JSON.parse(msg.toString())
-      if (!this.config.handlers.includes(type)) throw this.error('invalidWorkerHandler')
-      await runHook(`${this.name}:beforeWorkerStart`, msg)
-      await callHandler(this, type, payload, true)
-      await runHook(`${this.name}:afterWorkerEnd`, msg)
-    } catch (err) {
-      console.error(err)
-      this.log.error('jobQueueError%s', err.message)
-    }
-  }
-}
+import zmq from 'zeromq'
 
 async function factory (pkgName) {
   const me = this
@@ -26,8 +20,7 @@ async function factory (pkgName) {
       this.config = {
         localWorker: true,
         host: '127.0.0.1',
-        port: 7781,
-        handlers: ['masohi:send']
+        port: 7781
       }
     }
 
@@ -39,15 +32,38 @@ async function factory (pkgName) {
       this.puller = new zmq.Pull()
       this.puller.connect(`tcp://${this.config.host}:${this.config.port}`)
       this.log.debug('pullerStarted%s%s%d', this.name, this.config.host, this.config.port)
-      runner.call(this)
+
+      const runner = async () => {
+        const { runHook, callHandler } = this.app.bajo
+        for await (const [msg] of this.puller) {
+          try {
+            const options = JSON.parse(msg.toString())
+            const { worker, payload, source, callback } = options // see data format above
+            await runHook(`${this.name}:beforeWorkerStart`, options)
+            await callHandler(worker, { payload, source })
+            await runHook(`${this.name}:afterWorkerEnd`, options)
+            if (callback) await callHandler(callback, { payload, source })
+          } catch (err) {
+            this.log.error('jobQueueError%s', err.message)
+          }
+        }
+      }
+
+      runner()
     }
 
-    push = async (type, input) => {
-      const { isArray, isPlainObject } = this.app.bajo.lib._
-      const payload = isArray(input) || isPlainObject(input) ? input : { value: input }
-      const data = JSON.stringify({ type, payload })
+    push = async (options = {}) => {
+      const { runHook } = this.app.bajo
+      const { has } = this.lib._
+      const { worker, payload } = options
       try {
-        await this.pusher.send(data)
+        if (!worker) throw this.error('isRequired%s', this.print.write('worker'))
+        if (!payload) throw this.error('isRequired%s', this.print.write('payload'))
+        if (!(has(payload, 'data') && has(payload, 'type'))) throw this.error('isRequired%s', 'data, type')
+        if (payload.type === 'error') payload.data = payload.data.message
+        await runHook(`${this.name}:beforePush`, options)
+        await this.pusher.send(JSON.stringify(options))
+        await runHook(`${this.name}:afterPush`, options)
         return true
       } catch (err) {
         this.log.error('queueError%s', err.message)
