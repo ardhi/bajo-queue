03f30b60b2fdbe4af0cf3b32be53e4d95debb6b3
Ardhi Lukianto <ardhi@lukianto.com>
2025-08-23 23:31:58 +0700

    Updates

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ function reviver (key, value) {
 async function factory (pkgName) {
   const me = this
 
-  return class BajoQueue extends this.lib.BajoPlugin {
+  return class BajoQueue extends this.lib.Plugin {
     constructor () {
       super(pkgName, me.app)
       this.alias = 'q'
